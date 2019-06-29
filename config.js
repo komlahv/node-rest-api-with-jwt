19de0334cbdeccc5c8fcab54ec8e77493c55b448
Victor Eglein-Komlah <komlahv@gmail.com>
2019-06-29 18:20:17 +0000

    used env vars

--- config.js
+++ config.js
@@ -1,5 +1,9 @@
+let signature = process.env.signature; // gets secret key from env variable
+//open your cmd(I use cmdr) and type " set signature='yoursecretkey' "
+//you can always view the set value in cmd by typing "node" hit enter then type "process.env.signature"
+
 module.exports = {
-  'secret': 'supersecret'
+  'secret': signature
 };
 
-//NEVER DO THIS ALWAYS STORE SECRET KEY IN ENV 
+//ALWAYS STORE SECRET KEY IN ENV 
