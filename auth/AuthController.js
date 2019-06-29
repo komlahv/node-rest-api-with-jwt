241abd8e6ed04335b1abd7f513257ce624a2fbdf
Victor Eglein-Komlah <komlahv@gmail.com>
2019-06-29 14:17:11 +0000

    Added email check for already existing user

--- auth/AuthController.js
+++ auth/AuthController.js
@@ -15,12 +15,14 @@ var jwt = require('jsonwebtoken'); // used to create, sign, and verify tokens
 var bcrypt = require('bcryptjs');
 var config = require('../config'); // get config file
 
-router.post('/login', function(req, res) {
+
+
+router.post('/login', function (req, res) {
 
   User.findOne({ email: req.body.email }, function (err, user) {
     if (err) return res.status(500).send('Error on the server.');
     if (!user) return res.status(404).send('No user found.');
-    
+
     // check if the password is valid
     var passwordIsValid = bcrypt.compareSync(req.body.password, user.password);
     if (!passwordIsValid) return res.status(401).send({ auth: false, token: null });
@@ -37,34 +39,46 @@ router.post('/login', function(req, res) {
 
 });
 
-router.get('/logout', function(req, res) {
+
+
+router.get('/logout', function (req, res) {
   res.status(200).send({ auth: false, token: null });
 });
 
-router.post('/register', function(req, res) {
+router.post('/register', function (req, res) {
 
-  var hashedPassword = bcrypt.hashSync(req.body.password, 8);
+  User.findOne({ email: req.body.email }, function (err, user) {
+    if (err) return res.status(500).send('Error on the server.');
+    if (user) return res.status(404).send('User Already Exists.')
+    else {
+      var hashedPassword = bcrypt.hashSync(req.body.password, 8);
 
-  User.create({
-    name : req.body.name,
-    email : req.body.email,
-    password : hashedPassword
-  }, 
-  function (err, user) {
-    if (err) return res.status(500).send("There was a problem registering the user`.");
+      User.create({
+        name: req.body.name,
+        email: req.body.email,
+        password: hashedPassword
+      },
+        function (err, user) {
+          if (err) return res.status(500).send("There was a problem registering the user`.");
 
-    // if user is registered without errors
-    // create a token
-    var token = jwt.sign({ id: user._id }, config.secret, {
-      expiresIn: 86400 // expires in 24 hours
-    });
+          // if user is registered without errors
+          // create a token
+          var token = jwt.sign({ id: user._id }, config.secret, {
+            expiresIn: 86400 // expires in 24 hours
+          });
 
-    res.status(200).send({ auth: true, token: token });
+          res.status(200).send({ auth: true, token: token });
+        });
+    };
   });
+
+
 
 });
 
-router.get('/me', VerifyToken, function(req, res, next) {
+
+
+router.get('/me', VerifyToken, function (req, res, next) {
 
   User.findById(req.userId, { password: 0 }, function (err, user) {
     if (err) return res.status(500).send("There was a problem finding the user.");
